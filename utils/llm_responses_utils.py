d24e9b338e48ffb07ca0e83e78aff8d1cf84e047
Ding Peng <dingpeng@smail.nju.edu.cn>
2024-03-27 22:35:33 +0800

    Add files via upload

--- utils/llm_responses_utils.py
+++ utils/llm_responses_utils.py
@@ -29,5 +29,33 @@ def claude_responses(args, text: str):
                 args.claude_base_url)
     return model_output
 
-def llama2chat_responses():
+def llama2_responses(args, text: str):
     pass
+
+def mistral_responses(args, model, tokenizer, text: str):
+    user_input = [
+        {"role": "user", "content": text}
+    ]
+   
+
+    encodeds = tokenizer.apply_chat_template(user_input, return_tensors="pt")
+    model_inputs = encodeds.to("cuda")
+    model.to("cuda")
+
+    generated_ids = model.generate(model_inputs, pad_token_id=tokenizer.eos_token_id, max_new_tokens=args.max_tokens, do_sample=True)
+    decoded = tokenizer.batch_decode(generated_ids)
+
+    parts = decoded[0].split("[/INST] ")
+    if len(parts) > 1:
+        content_after_inst = parts[1]
+    else:
+        content_after_inst = ""
+    model_output = content_after_inst.replace("</s>", "")
+
+    return model_output
+    
+
+
+
+    pass
+    
